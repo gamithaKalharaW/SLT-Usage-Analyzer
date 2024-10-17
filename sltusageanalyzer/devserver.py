48e18abeb6732580a378ac2b8d992804f620c5e1
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-10-17 16:22:00 +0530

    devserver loglevel

--- sltusageanalyzer/devserver.py
+++ sltusageanalyzer/devserver.py
@@ -2,7 +2,10 @@ from sltusageanalyzer.__main__ import app
 
 from loguru import logger
 
+import sys
+
 
 if __name__ == "__main__":
+    logger.add(sys.stderr, format="{time} {level} {message}", level="DEBUG")
     logger.warning("Starting development server...")
     app.run(debug=True, host="0.0.0.0", port=3000)
