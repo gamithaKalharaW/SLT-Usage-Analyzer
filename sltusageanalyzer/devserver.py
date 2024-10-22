cdede8d05e2f3eb8cafc59c6ec0b65f4a472a501
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-10-22 14:10:10 +0530

    custom port for devserever
    added the ability to run the development userver using a user defined port

--- sltusageanalyzer/devserver.py
+++ sltusageanalyzer/devserver.py
@@ -6,6 +6,12 @@ import sys
 
 
 if __name__ == "__main__":
+    _port = None
+    if len(sys.argv) > 1:
+        try:
+            _port = int(sys.argv[1])
+        except ValueError:
+            _port = 3000
     logger.add(sys.stderr, format="{time} {level} {message}", level="DEBUG")
     logger.warning("Starting development server...")
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(debug=True, host="0.0.0.0", port=_port)
